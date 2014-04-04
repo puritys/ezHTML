24101ba51a1c951b0fa96f6845241094f886a236
puritys <pipi00000@gmail.com>
2014-04-04 13:39:06 +0800

    add unit test and bootstrap

--- tests/lib/ezHTML_generatorTests.js
+++ tests/lib/ezHTML_generatorTests.js
@@ -1,18 +1,90 @@
-require("phplike");
+
 
 var ez = require("../../lib/ezHTML_generator.js");
 global.window = [];
 
 
-var g = new ez();
+var ezHTML = new ez();
+/*
+
+require("phplike");
 var text = file_get_contents("text.txt");
 
 
-var section = g.getArticleSection(text);
+//var section = g.getArticleSection(text);
 
 //console.log(section);
-
-
-
 var html = g.transform(text);
-console.log(html);
+console.log(html);*/
+
+var isDebug = false;
+if (global.process.execArgv) {
+    var param = global.process.execArgv.join(" ");
+    if (param.match(/--debug/)) {
+        isDebug = true;
+    }
+}
+
+var assert = require("assert")
+
+//mocha lib/ --grep html
+describe('Normal html tag', function(){
+    it('Test # to h1', function(){
+        var text = '# h1 title';
+        var result = ezHTML.transform(text);
+        assert.equal("<h1>h1 title</h1>", result);
+    });
+
+    it('Test #h1 to h1', function(){
+        var text = '#h1 h1 title';
+        var result = ezHTML.transform(text);
+        assert.equal("<h1>h1 title</h1>", result);
+    });
+
+
+    it('Test ## to h2', function(){
+        var text = '## h2 title';
+        var result = ezHTML.transform(text);
+        assert.equal("<h2>h2 title</h2>", result);
+    });
+
+    it('Test #div to div', function(){
+        var text = '#div div content';
+        var result = ezHTML.transform(text);
+        assert.equal("<div>div content</div>", result);
+    });
+
+
+});
+
+
+
+
+//mocha lib/ --grep Image -d
+describe('Image html tag', function(){
+    it('Test #img to img', function () {
+        var text = '#img[100] ../img.jpg';
+        var result = ezHTML.transform(text);
+        if (isDebug) console.log(result);
+        assert.equal(1, result.match(/<img/).length);
+        assert.equal(1, result.match(/src="..\/img.jpg"/).length);      
+        assert.equal(1, result.match(/style="width: 100px;"/).length);      
+
+
+    });
+
+    it('Test #img to img with class name', function () {
+        var text = '#img[float-left] ../img.jpg';
+        var result = ezHTML.transform(text);
+        if (isDebug) console.log(result);
+        assert.equal(1, result.match(/class="float-left"/).length);
+
+
+    });
+
+});
+
+
+
+
+
