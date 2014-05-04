506d6630ca9881081ed7afdd689342fe5f071df9
puritys <pipi00000@gmail.com>
2014-05-04 21:51:54 +0800

    Add style and action of  demo

--- gui/ezHTML_GUI.js
+++ gui/ezHTML_GUI.js
@@ -16,6 +16,7 @@
 	
 	**/
 	function ezHTML_GUI(config) {//{{{
+        var foot = "";
 		this.config=config;
 		this.paragraph=Array();
 		this.editParagraphNum=-1;
@@ -30,17 +31,34 @@
 		if (this.config.iframe) {
 			var f = document.getElementById(this.config.iframe);
 			var head = '';
-			if(this.config.css){
+
+    		if (this.config.css) {
 				var n=this.config.css.length;
 				
 				for(var i=0 ; i<n;i++){
-					if(i>0){head+='\n';}
-					head+='<link href="'+this.config.css[i]+'" rel="stylesheet" type="text/css" />';
+					if(i>0) {
+                        head+='\n';
+                    }
+					head += '<link href="'+this.config.css[i]+'" rel="stylesheet" type="text/css" />';
 				}
 				
 			}
+
+    		if (this.config.js) {
+				n = this.config.js.length;
+				
+				for(i=0 ; i<n;i++){
+					if(i>0) {
+                        head+='\n';
+                    }
+					foot += '<script src="'+this.config.js[i]+'" type="application/javascript"></script>';
+				}
+				
+			}
+
+
 			f.contentWindow.document.open();
-			f.contentWindow.document.write('<html><head>'+head+'</head><body></body></html>');
+			f.contentWindow.document.write('<html><head>'+head+'</head><body></body> ' + foot + '</html>');
 			f.contentWindow.document.close();
 			//f.contentWindow.document.innerHTML='<html><head>'+head+'</head><body></body></html>';
 			
@@ -49,13 +67,13 @@
 	}//}}}
 	
 	var o = ezHTML_GUI.prototype;
-	o.setEvent=function(){
+	o.setEvent = function() {
 		$(this.config.source).keydown(this.keydown);
 	};
 
-	o.keydown=function(event) {
-		var code=event.keyCode;
-		if (code==9) {
+	o.keydown = function(event) {
+		var code = event.keyCode;
+		if (code == 9) {
 			var s = $(self.config.source);
 			self.insertAtCursor(s[0],'  ');
             event.preventDefault();
@@ -107,8 +125,8 @@
 
 		$("#"+this.config.destination).val(html);
 		if (this.config.iframe) {
-			var f=document.getElementById(this.config.iframe);
-			f.contentWindow.document.body.innerHTML=html;
+			var f = document.getElementById(this.config.iframe);
+			f.contentWindow.document.body.innerHTML = html;
 			//$('#'+this.config.iframe).html(s);
 		} else {
         	$("#"+this.config.destination_view).html(html);
@@ -116,7 +134,7 @@
 	};
 	
 	//轉換全部內容
-	o.transformAll = function() {
+	o.transformAll = function() {//{{{
 		if (self.editParagraphNum >= 0) {
 			//save content in the textarea
 			self.saveParagraph();
@@ -133,19 +151,19 @@
 		}
 
 		$(self.config.source).val(html);
-        //self.transform();
+        self.transform();
 		try{
-			self.transform();
+		//	self.transform();
 		} catch(err) {
             console.log(err);
 			alert("程式發生錯誤。");
 		}
-	};
+	};//}}}
 	
 	
 	
 	//Split code to multi paragraph by h2
-	o.splitParagraph=function(){
+	o.splitParagraph = function() {//{{{
 		var code = $(this.config.source).val();
 		var re=/[^#](##|#h2)[\s]/i, mat, mat2, code2, search, search2;
 		var re2=/([a-z]+)/, data;
@@ -187,10 +205,10 @@
 			data="";
 			
 		}
-	}
+	};//}}}
 	
 	/****抓下一個##的開始位置****/
-	o.getMenuStartPosition=function(mat,code){
+	o.getMenuStartPosition = function(mat,code) {
 		var re=/[^#](##|#h2)[\s]/;
 		var pos,len=0;
 		len=mat.index+mat[0].length+1;
@@ -222,14 +240,14 @@
 	}
 	
 	//輸出 各個片段的 move table
-	o.outputPieceTable=function(){
-		var html="", li, ul;
-		var n=this.paragraph.length,i,mat,title;
-		var re=/[\s]*(##|#h2)[\s]([^\n\r]+)/,p,a;
-		$("#"+self.config.paragraphMenu).html('');
+	o.outputPieceTable = function() {
+		var html = "", li, ul;
+		var n = this.paragraph.length,i,mat,title;
+		var re = /[\s]*(##|#h2)[\s]([^\n\r]+)/,p,a;
+		$("#" + self.config.paragraphMenu).html('');
         ul = document.createElement('ul');
         ul.className = "list-group";
-  		for (i=0;i<n;i++) {
+  		for (i = 0; i < n; i++) {
 			if (!this.paragraph[i].content || !this.paragraph[i].content.trim()) {
                 continue;
             }
@@ -269,28 +287,28 @@
 	    $("#"+this.config.paragraphMenu).append(ul);
 
 		a = document.createElement('a');
-		a.innerHTML="New Section";
-		a.href="#";
-		a.style.display="block";
-		a.style.marginTop="10px";
-		$(a).click(function(){
+		a.innerHTML = "New Section";
+		a.href = "#";
+		a.style.display = "block";
+		a.style.marginTop = "10px";
+		$(a).click(function() {
 			self.addNewParagraph();
 			return false;
 		});
-		$("#"+this.config.paragraphMenu).append(a);
+		$("#" + this.config.paragraphMenu).append(a);
 	};
 	
 	
 	/****段落處理****/
 	o.addNewParagraph=function(){
-		var n=self.paragraph.length;
-		self.paragraph[n]=Array();
-		self.paragraph[n].content="%---New Paragraph";
-		self.paragraph[n].type="";
-		self.outputPieceTable();
-		self.editParagraphNum=n;
-		self.editParagraph(n,'nosave');
-	}
+        var n=self.paragraph.length;
+        self.paragraph[n]=Array();
+        self.paragraph[n].content="%---New Paragraph";
+        self.paragraph[n].type="";
+        self.outputPieceTable();
+        self.editParagraphNum=n;
+        self.editParagraph(n,'nosave');
+	};
 	
 	//edit a paragraph
 	o.editParagraph=function(p,type) {
@@ -299,15 +317,17 @@
         }
 		$(self.config.source).val(self.paragraph[p].content.trim());
 		self.editParagraphNum=p;
-	}
+	};
+
 	//儲存當前內容進入目標 paragraph
 	o.saveParagraph=function(){
 		if(self.editParagraphNum>=0){
 			self.paragraph[self.editParagraphNum].content=$(self.config.source).val();	
 		}
 	};
+
 	//刪除段落
-	o.deleteParagraph=function(p){
+	o.deleteParagraph=function(p){//{{{
 		var n=self.paragraph.length;
 		var newp=Array();
 		for(var i=0;i<n;i++){
@@ -318,9 +338,9 @@
 		}
 		self.paragraph=newp;
 		self.outputPieceTable();
-	}
+	};//}}}
 	
-	o.paragraphContextMenu=function(event){
+	o.paragraphContextMenu = function(event) {//{{{
 
 		var t='<div class="_btnMenuBox">';
 		t+='<div class="btn_ambtn2"><a href="#" onclick="_callEditionUI(1);return false;">刪除</a></div>';
@@ -338,7 +358,7 @@
 		$("#_editionBtnBox").css({zIndex:"110",position:"absolute",left:l,top:t,display:"block"});
 		
 		return false;
-	}
+	};//}}}
 	
 	String.prototype.trim = function()
 	{
---
+++ gui/ezHTML_main.js
@@ -0,0 +1,48 @@
+(function() {
+
+/**
+ consturctor
+*/	
+function ezHTML_main() {
+
+}
+
+var o = ezHTML_main.prototype;
+o.prefix = "ezHTML";
+
+o.popup = function (E) {
+    var node, THIS, url, left = 300, top = 220, dataInfo;
+    THIS = E.data.context;
+    E.preventDefault();
+    E.stopPropagation();
+    
+    node = $(E.currentTarget);
+    url = node.attr('href');
+    dataInfo = node.attr('data-info');
+    eval("dataInfo = " + dataInfo);
+    if (dataInfo) {
+        if (dataInfo.width) {
+            left -= dataInfo.width/2;
+        }
+        if (dataInfo.height) {
+            top -= dataInfo.height/2;
+        }
+        if (top <= 0) top = 50;
+        if (left < 0) left = 50;
+    }
+    window.open(url, "ezHTML_popup", "width=300, height=200, resizable=yes, status=no, top=" + top + ", left=" + left);
+};
+
+o.initial = function () {
+    $('.' + this.prefix + '-popup').bind("click", {context: this}, this.popup);
+
+};
+
+var main = new ezHTML_main();
+$(document).ready(function () {
+    main.initial(); 
+});
+
+
+//end
+})();
